f7d7d93f90a41e0228cfee196bb192401e47a672
Martha Villalta <msv.ltmc@gmail.com>
2025-09-12 21:02:13 -0700

    Added basic main function to start Lab 6

---
+++ 210-lab-6.cpp
@@ -0,0 +1,19 @@
+/* Description: Create 3 functions (plus main, of course);
+* enterArrayData() = receives dynamic double array and pops. it
+w/vals. from user input.
+* outputArray() - receives dynamic double array & outputs its data
+on 1 line. (REMEMBER: JUST ONE)
+* sumArray() - receives dynamic doube arr. Calcs., and returns the sum.
+Will not output to console.
+In main()- Dynamic mem. allocation, create a double arr., SIZE 5.
+Use a -constant for integer literal. Pass the arr. to the 3 functions.
+
+Comsc-210 | Lab #6 | Martha Stephanie Villalta
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Lab 6 starting point!" << endl;
+    return 0;
+}
