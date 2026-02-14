c5f9df5de691644d48d5ab1624b0ee0e1f2974a1
msVill <msv.ltmc@gmail.com>
2026-02-14 14:35:08 -0800

    Added function definition, and will change my code as I go through creating the code logic because I feel that my function definitions are incorrect

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 using namespace std;
+// function: enterArrayData() - receives a dynamic double array
+// populates it with user entered data.
+// function: outputArrayData() - receives a dynamic double array
+// outputs the data of the double array to the console 
+// function: sumArray() - receives a dynamic double array
+// calculates and returns its sum. No output.
+// IN MAIN: use dynamic memory allocation
+// create array of doubles - size 5. use a constant
+// Pass the array to the 3 function to populate, output, and sum the array.
+// use pointer notaiton over array.
+
+const int SIZE = 5;
+
+// Function Prototypes:
+double * enterArrayData();
+void outputArrayData(double *);
+double * sumArray();
 
 int main() {
 
     return 0;
+}
+
+double * enterArrayData() {
+}
+
+void outputArrayData(double *) {
+
+}
+
+double * sumArray() {
+    
 }
